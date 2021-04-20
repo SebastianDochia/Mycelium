f5105749d76adff330a0d24416c5e7109d390561
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-04-20 18:34:06 +0300

    Added account creation route

--- MyceliumCore/models/User.js
+++ MyceliumCore/models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['sudent', 'profesor'],
+        enum: ['student', 'profesor'],
         default: 'user'
     },
     password: {
