2c950481ff9fb2e2123e64493f39bd43e221a866
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-04-20 20:21:42 +0300

    Added cookie handling

--- MyceliumCore/controllers/auth.js
+++ MyceliumCore/controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
+const { response } = require('express');
 
 // @desc    Register user
 // @route   POST /api/v1/auth/register
@@ -16,10 +17,7 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     });
 
-    // Create token
-    const token = user.getSignedJwtToken();
-
-    res.status(200).json({ success: true, token });
+    sendTokenResponse(user, 200, res);
 });
 
 // @desc    Login user
@@ -47,8 +45,21 @@ exports.login = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse('Invalid credentials', 401));
     }
 
+    sendTokenResponse(user, 200, res);
+});
+
+// Get token from model. create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
     // Create token
     const token = user.getSignedJwtToken();
 
-    res.status(200).json({ success: true, token });
-});
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOCKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token
+    });
+}
