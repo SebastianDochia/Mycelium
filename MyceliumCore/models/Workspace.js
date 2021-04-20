efcf45a37c6964f755ddd0c6f1a879e8e5129808
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-04-20 17:10:39 +0300

    Added bcryptjs and jsonwebtoken

--- MyceliumCore/models/Workspace.js
+++ MyceliumCore/models/Workspace.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const WorkspaceSchema = new mongoose.Schema({
     name: {
@@ -32,6 +33,12 @@ const WorkspaceSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     }
+});
+
+// Create bootcamp slug from name
+WorkspaceSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
 });
 
 module.exports = mongoose.model('Workspace', WorkspaceSchema);
