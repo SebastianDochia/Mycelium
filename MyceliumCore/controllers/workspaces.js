1f1b56da06384d770207bb680337c4ebd5be697f
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-04-20 15:28:36 +0300

    Applied DRY principle for controllers

--- MyceliumCore/controllers/workspaces.js
+++ MyceliumCore/controllers/workspaces.js
@@ -1,86 +1,67 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Workspace = require('../models/Workspace');
 
 // @desc    Get all workspaces
 // @route   GET /api/v1/workspaces
 // @access  Private
-exports.getWorkspaces = async (req, res, next) => {
-    try {
-        const workspaces = await Workspace.find(req.params.id);
+exports.getWorkspaces = asyncHandler(async (req, res, next) => {
+    const workspaces = await Workspace.find(req.params.id);
 
-        res.status(200).json({ success: true, data: workspaces });
-    } catch (error) {
-        next(error);
-    }
-}
+    res.status(200).json({ success: true, data: workspaces });
+});
 
 // @desc    Get single workspace
 // @route   GET /api/v1/workspaces/:id
 // @access  Private
-exports.getWorkspace = async (req, res, next) => {
-    try {
-        const workspace = await Workspace.findById(req.params.id);
+exports.getWorkspace = asyncHandler(async (req, res, next) => {
+    const workspace = await Workspace.findById(req.params.id);
 
-        if (!workspace) {
-            return next(new ErrorResponse(`Workspace not found with id ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: workspace });
-    } catch (error) {
-        next(error);
+    if (!workspace) {
+        return next(new ErrorResponse(`Workspace not found with id ${req.params.id}`, 404));
     }
-}
+
+    res.status(200).json({ success: true, data: workspace });
+});
 
 // @desc    Create new workspace
 // @route   POST /api/v1/workspaces
 // @access  Private
-exports.createWorkspace = async (req, res, next) => {
-    try {
-        const workspace = await Workspace.create(req.body);
+exports.createWorkspace = asyncHandler(async (req, res, next) => {
+    const workspace = await Workspace.create(req.body);
 
-        res.status(201).json({
-            success: true,
-            data: workspace
-        });
-    } catch (error) {
-        next(error);
-    }
-}
+    res.status(201).json({
+        success: true,
+        data: workspace
+    });
+});
 
 // @desc    Update workspace
 // @route   PUT /api/v1/workspaces/:id
 // @access  Private
-exports.updateWorkspace = async (req, res, next) => {
-    try {
-        const workspace = await Workspace.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-            useFindAndModify: false
-        });
+exports.updateWorkspace = asyncHandler(async (req, res, next) => {
+    const workspace = await Workspace.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
 
-        if (!workspace) {
-            return next(new ErrorResponse(`Workspace not found with id ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: workspace });
-    } catch (error) {
-        next(error);
+    if (!workspace) {
+        return next(new ErrorResponse(`Workspace not found with id ${req.params.id}`, 404));
     }
-}
+
+    res.status(200).json({ success: true, data: workspace });
+});
 
 // @desc    Delete workspace
 // @route   DELETE /api/v1/workspaces/:id
 // @access  Private
-exports.deleteWorkspace = async (req, res, next) => {
-    try {
-        const workspace = await Workspace.findByIdAndDelete(req.params.id);
+exports.deleteWorkspace = asyncHandler(async (req, res, next) => {
+    const workspace = await Workspace.findByIdAndDelete(req.params.id);
 
-        if (!workspace) {
-            return next(new ErrorResponse(`Workspace not found with id ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: {} });
-    } catch (error) {
-        next(error);
+    if (!workspace) {
+        return next(new ErrorResponse(`Workspace not found with id ${req.params.id}`, 404));
     }
-}
+
+    res.status(200).json({ success: true, data: {} });
+});
