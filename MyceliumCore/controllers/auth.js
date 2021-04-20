0e18534d19adf8e1a9c570950ca9e80e14a638b4
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-04-20 20:23:02 +0300

    Enabled secure flag for production on cookie

--- MyceliumCore/controllers/auth.js
+++ MyceliumCore/controllers/auth.js
@@ -11,9 +11,9 @@ exports.register = asyncHandler(async (req, res, next) => {
 
     // Create user
     const user = await User.create({
-        name, 
-        email, 
-        password, 
+        name,
+        email,
+        password,
         role
     });
 
@@ -27,21 +27,21 @@ exports.login = asyncHandler(async (req, res, next) => {
     const { email, password } = req.body;
 
     // Validate email and password
-    if(!email || !password) {
+    if (!email || !password) {
         return next(new ErrorResponse('Please Provide an email and password', 400));
     }
 
     // Check for user
     const user = await User.findOne({ email }).select('+password');
 
-    if(!user) {
+    if (!user) {
         return next(new ErrorResponse('Invalid credentials', 401));
     }
 
     // Check if password matches
     const isMatched = await user.matchPassword(password);
 
-    if(!isMatched) {
+    if (!isMatched) {
         return next(new ErrorResponse('Invalid credentials', 401));
     }
 
@@ -57,6 +57,10 @@ const sendTokenResponse = (user, statusCode, res) => {
         expires: new Date(Date.now() + process.env.JWT_COOCKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true
     };
+
+    if (process.env.NODE_ENV === ' production') {
+        options.secure = true;
+    }
 
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
