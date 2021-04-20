571b9c9459e522e008084f38b006f0dd9e747b54
SebastianDochia <44543115+SebastianDochia@users.noreply.github.com>
2021-04-20 15:16:57 +0300

    Added Mongoose validation error handling

--- MyceliumCore/middleware/error.js
+++ MyceliumCore/middleware/error.js
@@ -5,19 +5,30 @@ const errorHandler = (err, req, res, next) => {
 
     error.message = err.message;
 
-    //Log for dev
-    console.log(err.stack.red);
+    // Log for dev
+    console.log(err);
 
     // Mongoose bad ObjectId
     if(err.name === 'CastError') {
         const message = `Resource not found with id ${err.value}`;
         error = new ErrorResponse(message, 404);
     }
-    console.log(err.name);
+    
+    // Mongoose duplicate key
+    if(err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
 
-    res.status(err.statusCode || 500).json({
+    // Mongoose validation error
+    if(err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message || "Server Error"
+        error: error.message || "Server Error"
     });
 }
 
